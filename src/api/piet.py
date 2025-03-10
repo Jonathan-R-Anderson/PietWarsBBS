b10e594146519bc946a4bdbdb377e254406b53bc
Jonathan Anderson <jranderson404@gmail.com>
2025-03-09 20:22:10 -0400

    refatoring piet

--- src/api/piet.py
+++ src/api/piet.py
@@ -3,7 +3,7 @@ class PietInterpreter:
 
     # Piet color cycle
     PIET_COLORS = [
-        "\033[31m", "\033[33m", "\033[32m", "\033[36m", "\033[34m", "\033[35m"
+        "\\033[31m", "\\033[33m", "\\033[32m", "\\033[36m", "\\033[34m", "\\033[35m"
     ]
 
     def __init__(self, board, colors):
